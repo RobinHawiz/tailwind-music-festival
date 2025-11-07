411286fbfedb2b7723e5a26c578d2d1b6254ac74
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2025-11-07 11:29:39 +0100

    ✨ feat: add responsive navbar with submenus

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
 import "/styles/style.css";
+
+window.onload = () => {
+  const mobileMenuIcon = document.getElementById("mobile-menu-button");
+  mobileMenuIcon?.addEventListener("click", () => {
+    mobileMenuIcon.classList.toggle("open");
+  });
+
+  const ticketMenuButton = document.getElementById("ticket-menu-button");
+  ticketMenuButton?.addEventListener("click", () => {
+    ticketMenuButton.classList.toggle("open");
+  });
+};
